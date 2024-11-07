edf61b2362047ec25c63e47c9a2c4c33a3c9b287
sakhezech <contact@sakhezech.ru>
2024-11-07 15:46:29 +0300

    feat: new cli

--- src/pagebuilder/__main__.py
+++ src/pagebuilder/__main__.py
@@ -1,82 +1,120 @@
-import dataclasses
-import typing
 from argparse import ArgumentParser
-from collections.abc import Callable
-from types import NoneType
-from typing import Concatenate
+from collections.abc import Sequence
+from pathlib import Path
+
+from .__version__ import __version__
+from .builder import PageBuilder, serve
 
 
-def _add_args_from[**Pargs, Targs, **Pwrap, Twrap](_: Callable[Pargs, Targs]):
-    type Combined = Callable[Concatenate[Callable[Pwrap, Twrap], Pargs], Targs]  # type: ignore
+def cli(argv: Sequence[str] | None = None) -> None:
+    parser = ArgumentParser(
+        prog='pagebuilder',
+        description='a static site generator i built',
+    )
 
-    def wrapper(func_to_wrap: Combined) -> Combined:
-        return func_to_wrap
+    parser.add_argument(
+        '-v',
+        '--version',
+        action='version',
+        version=__version__,
+    )
 
-    return wrapper
+    parser.add_argument(
+        '-w',
+        '--watch',
+        metavar='ADDR',
+        nargs='?',
+        const='localhost:5000',
+        default=None,
+        help="watcher mode (serves by default on 'localhost:5000')",
+    )
 
+    parser.add_argument(
+        '--serve-dir',
+        type=Path,
+        default=None,
+        help="""
+        directory to serve from
+        (use if impossible to imply from where to serve)
+        """,
+    )
 
-@dataclasses.dataclass
-class _Hint:
-    name: str
-    class_: Callable
-    default: object
+    builder_select_group = parser.add_mutually_exclusive_group()
 
+    builder_select_group.add_argument(
+        '--args',
+        metavar='ARG',
+        nargs='*',
+        help="""
+        list of arguments for a builder in order 
+        (pages_path,
+        templates_path,
+        assets_path,
+        dist_path,
+        extension (defaults to '.html'),
+        data_start (defaults to '<!-- YAML:\\n'),
+        data_end (defaults to '-->\\n'))
+        """,
+    )
 
-@_add_args_from(ArgumentParser)
-def make_parser(func: Callable, *args, **kwargs) -> ArgumentParser:
-    EMPTY = object()
+    builder_select_group.add_argument(
+        '-b',
+        '--builder',
+        action='append',
+        help="""
+        builder instance to run, in the format '<module>:<instance>'
+        (can be used multiple times)
+        """,
+    )
 
-    raw_hints = typing.get_type_hints(func)
-    raw_hints.pop('return', None)
-    defaults = func.__defaults__ or tuple()
-
-    hints = [_Hint(name, class_, EMPTY) for name, class_ in raw_hints.items()]
-    for hint_with_def, default in zip(hints[-len(defaults) :], defaults):
-        hint_with_def.default = default
-
-    parser = ArgumentParser(*args, **kwargs)
-    for hint in hints:
-        if not callable(hint.class_):
-            possible_classes = [
-                possible
-                for possible in typing.get_args(hint.class_)
-                if possible is not NoneType and callable(possible)
-            ]
-
-            if hint.default is EMPTY:
-                raise ValueError('union types without defaults not allowed')
-            if len(possible_classes) != 1:
-                raise ValueError('unclear which callable use as constructor')
-
-            hint.class_ = possible_classes[0]
-
-        if hint.default is EMPTY:
-            parser.add_argument(hint.name, type=hint.class_)
-        else:
-            parser.add_argument(
-                f'--{hint.name}', type=hint.class_, default=hint.default
+    args = parser.parse_args(argv)
+    builders: list[PageBuilder] = []
+    if args.builder:
+        for builder_path in args.builder:
+            import_path, _, builder_name = builder_path.partition(':')
+            module = __import__(import_path)
+            builder = getattr(module, builder_name, None)
+            if not isinstance(builder, PageBuilder):
+                raise ValueError(f'not a builder: {builder}')
+            builders.append(builder)
+    else:
+        if not 4 <= len(args.args) <= 7:
+            raise ValueError(
+                'number of arguments should be between '
+                f'4 and 7: {len(args.args)}'
             )
+        paths = [Path(p) for p in args.args[:4]]
+        kwargs = {
+            key: val
+            for key, val in zip(
+                ('ext', 'data_start', 'data_end'), args.args[4:]
+            )
+        }
+        builder = PageBuilder(*paths, **kwargs)
+        builders.append(builder)
 
-    return parser
+    if args.watch:
+        if not args.serve_dir:
+            if len(builders) == 1:
+                args.serve_dir = builders[0].dist_path
+            else:
+                raise ValueError(
+                    'with 2 or more builders and no --serve-dir '
+                    'impossible to imply from where to serve'
+                )
+
+        addr, _, port = args.watch.partition(':')
+        try:
+            for builder in builders:
+                builder.observe()
+            serve(addr, int(port), args.serve_dir)
+        finally:
+            for builder in builders:
+                builder.stop_observing()
+    else:
+        for builder in builders:
+            builder.build()
 
 
 if __name__ == '__main__':
-    from .__version__ import __version__
-    from .builder import PageBuilder, serve
-
-    parser = make_parser(PageBuilder.__init__, prog='pagebuilder')
-    parser.add_argument('-a', '--addr', default=None)
-    parser.add_argument(
-        '-v', '--version', action='version', version=__version__
-    )
-    args = parser.parse_args()
-    builder_args = args.__dict__.copy()
-    addr_port: str = builder_args.pop('addr')
-
-    builder = PageBuilder(**builder_args)
-    if addr_port:
-        addr, _, port = addr_port.partition(':')
-        with builder:
-            serve(addr, int(port), builder.dist_path)
-    else:
-        builder.build()
+    cli()
